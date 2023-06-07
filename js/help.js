f662bb20b84fdd3798383c73a5ef3b4005036a00
Ilia Filimonov <9991571155i@gmail.com>
2023-06-07 19:12:34 +0400

    пример карточек с динамической подгрузкой. добавил обработку ошибок

--- js/help.js
+++ js/help.js
@@ -48,9 +48,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const tabsBtn = document.querySelectorAll('.tabs-btn');
   const tabsContent = document.querySelectorAll('.tabs-content');
 
-  if(tabs) {
+  if (tabs) {
     tabs.addEventListener('click', (e) => {
-      if(e.target.classList.contains('tabs-btn')) {
+      if (e.target.classList.contains('tabs-btn')) {
         const tabsPath = e.target.dataset.tabsPath;
         tabsBtn.forEach(el => el.classList.remove('tabs-btn__active'));
         document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs-btn__active');
@@ -64,3 +64,57 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs-content__active');
   }
 })
+
+
+
+/* карточки */
+const fetchData = () => {
+  return fetch('../data.json')
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return res.json();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      return [];
+    });
+};
+
+const renderData = async () => {
+  try {
+    const products = await fetchData();
+    const productsWrapper = document.querySelector('.products-wrapper');
+
+    if (productsWrapper) {
+      products.forEach(product => {
+        const div = document.createElement('div');
+        div.classList.add('card-wrapper');
+
+        div.innerHTML = `
+          <a class="card-link" href="#" target="_blank" rel="noopener noreferrer nofollow"></a>
+          <div class="card">
+            <div class="card-image">
+              <img src="${product.imgSrc}" alt="image">
+            </div>
+            <div class="card-text__descr text-secondary">${product.rating}</div>
+            <div class="card-text__config text-secondary">${product.name}</div>
+            <div class="card-text__date text-secondary">${product.price}</div>
+          </div>
+        `;
+
+        productsWrapper.appendChild(div);
+      });
+    } else {
+      console.error('Element with class "products-wrapper" not found');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+
+document.addEventListener('DOMContentLoaded', renderData);
+
+
